6c33c2c43019c676cd4443ec0ad5915b6db4937e
Jamie Jenks <jenksjm@gmail.com>
2022-11-30 16:03:55 +0100

    add command line arguments

---
+++ script.py
@@ -0,0 +1,3 @@
+website = input("What is the name of your website? ")
+keyword = input("What keyword would you like to search for? ")
+print ('Searching Google for keyword ', keyword, 'and website ', website)
