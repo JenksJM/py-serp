83989ca8a835bc6b54787378eff93e7e731e3182
Jamie Jenks <jenksjm@gmail.com>
2022-11-30 16:54:45 +0100

    update google search package

--- script.py
+++ script.py
@@ -10,9 +10,8 @@ website = input("What is the name of your website? ").strip()
 keyword = input("What keyword would you like to search for? ").strip()
 print ('Searching Google for keyword ', keyword, 'and website ', website)
 
-googleResults = search(keyword, num_results=10, lang="en")
-for index, result in enumerate(googleResults):
-    parsed_url = urlparse(result)
+for index, url in enumerate(search(keyword, tld="co.uk", num=10, stop=10, pause=2)):
+    parsed_url = urlparse(url)
     if website == parsed_url.netloc:
         print("Found at postion: ", index + 1)
         sys.exit()
